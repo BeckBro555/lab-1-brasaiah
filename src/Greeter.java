4ea0825c422b32dcb3c6c18fd7fbef5c9829e078
rasa1631 <BeckBro555@gmail.com>
2023-02-23 10:58:00 -0500

    commit

--- src/Greeter.java
+++ src/Greeter.java
@@ -6,47 +6,54 @@
  */
 
 //import javax.swing.JOptionPane;
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Greeter {
 
-    // TODO: return the name entered by the user
-    // QUESTION: Why can this method be marked as private?
-    public static void main(String[] args){
-        //System.out.println("What is your name");
-        //getNameFromGui();
-        //System.out.println("Hello" + name);
-        //JOptionPane.showInputDialog
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("What is your name");
-        String Name = myObj.nextLine();  // Read user input
-        System.out.println("Hello " + Name + "!");
+    private static String GetNameFromGui() {
+        String name = JOptionPane.showInputDialog("Tell me your name good sir or maam, or xi or xer. Idk man this generation is pretty wild");
+        System.out.println(name);
+        return name;
     }
-     //public static void PrintName(){
 
-    }
+
 
     // TODO: write a printName method that prints the messages "Hello, {name}!" to the console
 
-//public static void main(String[] args){
-        //name = getNameFromGui();
-       //System.out.println("Hello" + getNameFromGui());
+    public static void PrintName(String name) {
+        System.out.println("Hello " + name+". Good to know that you exist. I would like to reach out to you about your car's extended warranty.");
+    }
 
 
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
+        String name = GetNameFromGui();
+        PrintName(name);
+    }
+}
+
+
 
         //TODO: call the getNameFromGui method and store the result in a variable
     //public class getNameFromGui(){
-
+//forget this stuff below this line this is just my trial and error
      //public static void main(String[] args) {
         //getNameFromGui();
+         //public static void main(String[] args){
+         //System.out.println("What is your name");
+         //getNameFromGui();
+         //System.out.println("Hello" + name);
+         //JOptionPane.showInputDialog
+         //Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+         //System.out.println("What is your name");
+         //String Name = myObj.nextLine();  // Read user input
+         //System.out.println("Hello " + Name + "!");
+//this scanner command is lame bc it was the first solution i found, so I did not use it
 
 
 
 
-        //int num = 7;
-        //int tenAdded = addTen(7);
-        //System.out.println("");
+
 
 
 
